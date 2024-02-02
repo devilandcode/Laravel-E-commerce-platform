1f71267317ddc172a32d62400d6664736fa05b66
devilandcode <devilandcode@proton.me>
2024-02-02 15:31:12 +0300

    Add github actions CI

--- resources/views/account/adverts/_nav.blade.php
+++ resources/views/account/adverts/_nav.blade.php
@@ -2,7 +2,7 @@
     <li class="nav-item"><a class="nav-link" href="{{ route('account.home') }}">Dashboard</a></li>
     <li class="nav-item"><a class="nav-link active" href="{{ route('account.adverts.index') }}">Adverts</a></li>
     <li class="nav-item"><a class="nav-link" href="#">Favorites</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Banners</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ route('account.banners.index') }}">Banners</a></li>
     <li class="nav-item"><a class="nav-link" href="{{ route('account.profile.home') }}">Profile</a></li>
     <li class="nav-item"><a class="nav-link" href="#">Tickets</a></li>
 </ul>
