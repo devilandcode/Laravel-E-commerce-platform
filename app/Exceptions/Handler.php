b03b9f02e43693a28ca991bd27dc38672595ba4c
devilandcode <devilandcode@proton.me>
2024-02-04 20:22:27 +0300

    Modify Exeption Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +26,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function(\DomainException $e, Request $request) {
+           if ($request->expectsJson()) {
+               return response()->json([
+                   'message' => $exception->getMessage(),
+               ], Response::HTTP_BAD_REQUEST);
+           }
         });
     }
 }
